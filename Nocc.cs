8225497a01f00df2405c3f89839dd68dbad8a2c3
rkc91 <rcasey391@gmail.com>
2025-01-08 20:05:05 -0500

    init commit

---
+++ Nocc.cs
@@ -0,0 +1,4 @@
+// Read the documentation https://botforgrindfest.com/ to learn how to write bots.
+
+Hero.Say("Hello, world!");
+//Hero.FollowCursorAndAttack(); // Uncomment this line to make the bot follow your mouse cursor and attack nearby enemies.
